6f7a70c2632d7b142d1438a5d5105306e92877da
Nurai <nurai.ulanbek1@gmail.com>
2024-01-25 14:43:50 +0600

    create partners

---
+++ src/components/Partners/Partners.tsx
@@ -0,0 +1,14 @@
+import css from "./Partners.css"
+
+export const Partners = () => {
+    return (
+        <div className="header">
+            <h1>Наши партнеры</h1>
+            <div className="partners-icons">
+                <img src="https://tutor.caedmi.com/wp-content/uploads/2023/10/community-chest-2.png" alt="Community Chest of Korea" />
+                <img src="https://tutor.caedmi.com/wp-content/uploads/elementor/thumbs/khayah-logo-q9ecp04b6y6ow90xcjd2488e65o2mzq504yq356dsg.png" alt="Khaya.kg" />
+                <img src="https://tutor.caedmi.com/wp-content/uploads/elementor/thumbs/caedmi-logo-q9ecp04872e2cd2opwib4s58p2kuz8hyauy6xavytw.png" alt="caedmi" />
+            </div>
+        </div>
+    )
+}
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,12 +1,14 @@
 import { Circle } from "../../components/Circle/Circle"
 import { MainBlock } from "../../components/MainBlock/MainBlock"
 import { RepBlock } from "../../components/RepBlock/RepBlock"
+import { Partners } from "../../components/Partners/Partners"
 export const HomePage = () => {
   return (
     <div>
         <MainBlock/>
         <Circle/>
         <RepBlock/>
+        <Partners/>
     </div>
   )
 }
